27909d35605ae09ca94a6a4f6c4d248af19d8af7
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2023-05-19 09:08:12 -0300

    teste para ExceptionHandler

---
+++ src/test/java/io/github/AugustoMello09/resource/exception/ResourceExceptionHandlerTeste.java
@@ -0,0 +1,60 @@
+package io.github.AugustoMello09.resource.exception;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import io.github.AugustoMello09.resources.exceptions.ResourceExceptionHandler;
+import io.github.AugustoMello09.resources.exceptions.StandardError;
+import io.github.AugustoMello09.services.exception.DataIntegratyViolationException;
+import io.github.AugustoMello09.services.exception.ObjectNotFoundException;
+
+@SpringBootTest
+public class ResourceExceptionHandlerTeste {
+	
+	private static final String EMAIL_JA_EXISTE = "Email já existe";
+	private static final String NAO_ENCONTRADO = "não encontrado";
+	@InjectMocks
+	private ResourceExceptionHandler exceptionHandler;
+	
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
+	}
+	
+	@Test
+	void whenObjectNotFoundThenReturnAResponseEntity() {
+		ResponseEntity<StandardError> response = exceptionHandler.objectNotFound(new ObjectNotFoundException(NAO_ENCONTRADO)
+				, new MockHttpServletRequest());
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+		assertEquals(ResponseEntity.class, response.getClass());
+		assertEquals(StandardError.class, response.getBody().getClass());
+		assertEquals(NAO_ENCONTRADO, response.getBody().getError());
+		assertEquals(404, response.getBody().getStatus());
+		
+	}
+	
+	@Test
+	void whenDataIntegraty() {
+		ResponseEntity<StandardError> response = exceptionHandler
+				.dataIntegraty(new DataIntegratyViolationException(EMAIL_JA_EXISTE)
+				, new MockHttpServletRequest());
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+		assertEquals(ResponseEntity.class, response.getClass());
+		assertEquals(StandardError.class, response.getBody().getClass());
+		assertEquals(EMAIL_JA_EXISTE, response.getBody().getError());
+		assertEquals(400, response.getBody().getStatus());
+	}
+}
